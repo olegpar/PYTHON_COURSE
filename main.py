33729f12ad9c5398c37c5f0fd8fedd6814495750
Oleg Parfenyuk <olegpar@yandex.ru>
2023-02-18 20:24:43 +0300

    Python_HW1

---
+++ main.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5.89
+c = 'hello'
+
+print(f"{a} - {b} - {c}") # или
+print("{} - {} - {}".format(a,b,c)) 
+
+print ('Введите первое число: ')
+a1 = int(input())
+b1 = int(input('Введите второе число: '))
+print(a1, '+', b1, '=', a1 + b1)
